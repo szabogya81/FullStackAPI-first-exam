f7746c64129bd443c8da7757f24dc98d928169f0
RaiN <szabogya81@gmail.com>
2021-07-05 15:08:55 +0200

    task_01

--- task-01/fn.js
+++ task-01/fn.js
@@ -5,7 +5,7 @@
  * @returns a tömbnek az az eleme, amelynek az id -je megegyezik a kapottal
  */
 const get = (list = [], id = 0) => {
-    //
+    return list.find( item => item.id === id );
 };
 
 /**
@@ -15,7 +15,10 @@ const get = (list = [], id = 0) => {
  * @returns a létrehozott, beszúrt és id -vel ellátott objektum
  */
 const create = (list = [], entity = null) => {
-    //
+    const id = list[list.length - 1].id + 1;
+    const newEntity = {...entity, id};
+    list.push( newEntity );
+    return newEntity;
 };
 
 /**
@@ -25,7 +28,19 @@ const create = (list = [], entity = null) => {
  * @returns a frissített objektum ha sikerült a frissítés, egyébként false
  */
 const update = (list = [], entity = {}) => {
-    //
+
+    if(list.length > 0 && entity?.id) {
+        try {
+            const index = list.findIndex( item => item.id === entity.id );
+            list[index] = {...list[index], ...entity};
+            return list[index];
+        } catch (err) {
+            console.log(err.message);
+            return false;
+        }
+    }
+
+    return false;
 };
 
 /**
@@ -35,9 +50,26 @@ const update = (list = [], entity = {}) => {
  * @returns true ha sikeres volt a törlés, egyébként false
  */
 const remove = (list = [], id = 0) => {
-    //
+    if(list.length > 0 && id) {
+        try {
+            const index = list.findIndex( item => item.id === id );
+            list.splice(index, 1);
+            return true;
+        } catch (err) {
+            console.log(err.message);
+            return false;
+        }
+    }
+
+    return false;
 };
 
 /**
  * 5. Exportáld ki a négy függvényt, hogy más fájlokból is elérhetőek legyenek.
  */
+ module.exports = {
+    get,
+    create,
+    update,
+    remove,
+};
